431a186342a1c536bcc06a943cb382f5e1119aaf
scottjehl <scott@scottjehl.com>
2012-03-07 20:56:51 +0530

    whitespace

--- js/jquery.mobile.fixedToolbar.polyfill.js
+++ js/jquery.mobile.fixedToolbar.polyfill.js
@@ -6,56 +6,56 @@ define( [ "jquery", "./jquery.mobile.fixedToolbar" ], function( $ ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
 				
-		var supportBlacklist = $.mobile.fixedtoolbar.prototype.options.supportBlacklist;
+	var supportBlacklist = $.mobile.fixedtoolbar.prototype.options.supportBlacklist;
 					
-		// If the browser is blacklisted for position:fixed support, polyfill it
-		if( supportBlacklist() ){
+	// If the browser is blacklisted for position:fixed support, polyfill it
+	if( supportBlacklist() ){
 				
-			// Add class for CSS hookage 
-			$( document.documentElement ).addClass( "ui-faux-fixed" );
+		// Add class for CSS hookage 
+		$( document.documentElement ).addClass( "ui-faux-fixed" );
 				
-			// Make the blacklist test return false, letting any normally-blacklisted browsers in to be polyfilled
-	  		$.mobile.fixedtoolbar.prototype.options.supportBlacklist = function(){ return false; };
+		// Make the blacklist test return false, letting any normally-blacklisted browsers in to be polyfilled
+  		$.mobile.fixedtoolbar.prototype.options.supportBlacklist = function(){ return false; };
 					
-			// Per page show, re-set up the event handling
-			$( document ).bind( "pagebeforeshow", function( e ){
+		// Per page show, re-set up the event handling
+		$( document ).bind( "pagebeforeshow", function( e ){
 						
-				var page = $( e.target ),
-					fixies = page.find( ".ui-header-fixed, .ui-footer-fixed" ),
-					visibleAtStart,
-					resetPos = function(){
-						fixies.each(function(){
-							if( $(this).hasClass( "ui-header-fixed") ){
-								$( this ).css( "top", $( window ).scrollTop() + "px" );
-							}
-							else {
-								$( this ).css( "bottom", $.mobile.activePage.height() - $( window).scrollTop() - $.mobile.getScreenHeight() + "px" );
-							}
-						});
-					};
-						
-				// Normalize proper object for scroll event
-				( ( $( document ).scrollTop() === 0 ) ? $( window ) : $( document ) )
-					.bind( "scrollstart.fixed", function(){
-						visibleAtStart = fixies.not( ".ui-fixed-hidden" ).fixedtoolbar( "hide", true );
-					})
-					.bind( "scrollstop.fixed", function(){
-						resetPos();
-						visibleAtStart.fixedtoolbar( "show" );
+			var page = $( e.target ),
+				fixies = page.find( ".ui-header-fixed, .ui-footer-fixed" ),
+				visibleAtStart,
+				resetPos = function(){
+					fixies.each(function(){
+						if( $(this).hasClass( "ui-header-fixed") ){
+							$( this ).css( "top", $( window ).scrollTop() + "px" );
+						}
+						else {
+							$( this ).css( "bottom", $.mobile.activePage.height() - $( window).scrollTop() - $.mobile.getScreenHeight() + "px" );
+						}
 					});
+				};
 						
-				// on resize, reset positioning
-				$( window ).bind( "throttledresize.fixed", resetPos );
-
-				// on pagehide, unbind the event handlers
-				page.one( "pagehide", function(){
-					$( window ).add( this ).add( document ).unbind( ".fixed" );
+			// Normalize proper object for scroll event
+			( ( $( document ).scrollTop() === 0 ) ? $( window ) : $( document ) )
+				.bind( "scrollstart.fixed", function(){
+					visibleAtStart = fixies.not( ".ui-fixed-hidden" ).fixedtoolbar( "hide", true );
+				})
+				.bind( "scrollstop.fixed", function(){
+					resetPos();
+					visibleAtStart.fixedtoolbar( "show" );
 				});
 						
-				// align for pageshow
-				resetPos();
+			// on resize, reset positioning
+			$( window ).bind( "throttledresize.fixed", resetPos );
+
+			// on pagehide, unbind the event handlers
+			page.one( "pagehide", function(){
+				$( window ).add( this ).add( document ).unbind( ".fixed" );
 			});
-		}
+						
+			// align for pageshow
+			resetPos();
+		});
+	}
 
 })( jQuery );
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
